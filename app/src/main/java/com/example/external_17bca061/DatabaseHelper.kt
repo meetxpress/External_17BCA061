a14c672493316cb8f6e10bf0f2c27836a74dc5d6
Meet Patel <pmeet7898@gmail.com>
2020-05-05 18:12:08 +0530

    External Exam

--- app/src/main/java/com/example/external_17bca061/DatabaseHelper.kt
+++ app/src/main/java/com/example/external_17bca061/DatabaseHelper.kt
@@ -11,17 +11,21 @@ class DatabaseHelper(var context: Context): SQLiteOpenHelper(context, DATABASE_N
     companion object{
         var DATABASE_NAME = "DB_Externals"
         var VERSION = 1
-        var TB_NAME = "User_Master"
+        var TB_NAME = "User_Master"             //for query
+       // var TB_NAME2 = "Details_Master"       //for query2
         var uId = "userId"
         var uName = "userName"
         var uEmail = "userEmail"
         var uMob = "userMob"
         var uPass = "userPassword"
     }
-//WHERE ($uEmail LIKE ('%@%.com'))
+
     override fun onCreate(db: SQLiteDatabase?) {
         var query="CREATE TABLE $TB_NAME($uId INTEGER PRIMARY KEY AUTOINCREMENT, $uName VARCHAR(20), $uEmail VARCHAR(50), $uMob VARCHAR(10), $uPass VARCHAR(15))"
         db!!.execSQL(query)
+
+        //var query2="CREATE TABLE $TB_NAME2($uId INTEGER PRIMARY KEY AUTOINCREMENT, $uName VARCHAR(20), $uEmail VARCHAR(50), $uMob VARCHAR(10), $uPass VARCHAR(15))"
+        //db!!.execSQL(query2)
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
@@ -34,7 +38,6 @@ class DatabaseHelper(var context: Context): SQLiteOpenHelper(context, DATABASE_N
         var db=writableDatabase
         var cv= ContentValues()
 
-        //cv.put(animal_id, aId)
         cv.put(uName, user_name)
         cv.put(uEmail, user_email)
         cv.put(uMob, user_mob)
@@ -55,7 +58,7 @@ class DatabaseHelper(var context: Context): SQLiteOpenHelper(context, DATABASE_N
         val selection = "$uId = ? and $uPass = ?"
         val selectionArgs = arrayOf(username, password)
         val cursor: Cursor =
-        db.query(TB_NAME, columns, selection, selectionArgs, null, null, null)
+        db.query("User_Master", columns, selection, selectionArgs, null, null, null)
         val count = cursor.count
         cursor.close()
         close()
